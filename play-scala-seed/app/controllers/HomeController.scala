4eb115d68a4ed911a4f3fcd7ba84451044c343f8
Alejandro Mena <alexandro.ramr777@gmail.com>
2024-12-09 17:12:48 -0500

    #2 render items from harcoded list

--- play-scala-seed/app/controllers/HomeController.scala
+++ play-scala-seed/app/controllers/HomeController.scala
@@ -4,6 +4,9 @@ import javax.inject._
 import play.api._
 import play.api.mvc._
 
+//import java.io.File
+//import com.github.tototoshi.csv._
+
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
@@ -20,5 +23,18 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
    */
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
+  }
+
+  def printSum(first: Long, second: Long) = Action { implicit request: Request[AnyContent] =>
+    val sum = first + second
+    Ok(views.html.index())
+  }
+
+  def dummy() = Action { implicit request: Request[AnyContent] =>
+    val elements: List[(String,String)] = List(
+      ("Iraq: elezioni, dopo denunce brogli domani nuovi conteggi","https://cdn.tuttosport.com/img/600/400/2021/10/26/103904635-f90f58ee-f6d4-4002-bf78-7eac86700221.jpg"),
+      ("Brahim Diaz negativo al Covid 19: il Milan e Pioli possono sorridere","https://cdn.tuttosport.com/img/600/400/2021/09/27/124308247-66ca927f-9779-4626-99ab-947537a599b3.jpg"),
+      ("Le staffette del Torino: con Juric le riserve sono un valore aggiunto","https://cdn.tuttosport.com/img/600/400/2021/10/26/103405207-5775923d-0ef1-4290-b227-937796728bbe.png"))
+    Ok(views.html.dummy(elements))
   }
 }
